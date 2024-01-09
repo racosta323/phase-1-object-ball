79a5fd21e95fd8a1cea5511502b2cd4008a2ab01
Rene <racosta323@gmail.com>
2024-01-09 14:23:27 -0600

    adds first object

---
+++ src/00-objectball.js
@@ -0,0 +1,34 @@
+   function gameObject(){
+    home: {
+        teamName: {""},
+        colors: [...],
+        players: {
+            "Alan Anderson": {...},
+            "Reggie Evans": {...},
+            "Brook Lopez":
+            "Mason Plumlee":
+            "Jason Terry":
+        },
+    }
+    away: {
+        ...
+    }
+    }
+
+
+
+
+
+//   {
+//     home: {
+//       teamName: "",
+//       colors: [...],
+//       players: {
+//         "Alan Anderson": {...},
+//         "Reggie Evans": {...}
+//       }
+//     },
+//     away: {
+//       ...
+//     } 
+//   }
