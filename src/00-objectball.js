ff86102f40f068c60e3c0fc1fbc7eea154f396f0
Rene <racosta323@gmail.com>
2024-01-09 15:03:53 -0600

    cleans code with Emiley help

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,32 +1,35 @@
    function gameObject(){
-    home: {
-         "teamName"= {"Brooklyn Nets"},
-         "colors" : ["Black", "White"],
-         "players": {
-            "Alan Anderson": {
-                "number":0,
-                "shoe":16,
-                "points":22,
-                "rebounds":12,
-                "assists":12,
-                "steals":3,
-                "blocks":1,
-                "slamDunks":1,
+        return {
+            "home": {
+                "teamName": "Brooklyn Nets",
+                "colors" : ["Black", "White"],
+                "players": {
+                    "Alan Anderson": {
+                        "number":0,
+                        "shoe":16,
+                        "points":22,
+                        "rebounds":12,
+                        "assists":12,
+                        "steals":3,
+                        "blocks":1,
+                        "slamDunks":1,
+                    },
+                    "Reggie Evans": {
+                        "number":0,
+                        "shoe":16,
+                        "points":22,
+                        "rebounds":12,
+                        "assists":12,
+                        "steals":3,
+                        "blocks":1,
+                        "slamDunks":1,
+                    },
                 },
-            "Reggie Evans": {
-               
-                
-               },
-              
-            
-                },
-        },
+            },
+            "away": {
+            }
+        }
     }
-    away: {
-        ...
-    }
-    }
-
 
 
 
