6823f68b4b1e9447a067ba50c505beac481f7a0a
Ikram Chouichi <ikram@netronian.com>
2024-01-09 15:39:59 -0500

    ADD more to object

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,13 +1,25 @@
    function gameObject(){
     home: {
-        teamName: {""},
-        colors: [...],
-        players: {
-            "Alan Anderson": {...},
-            "Reggie Evans": {...},
-            "Brook Lopez":
-            "Mason Plumlee":
-            "Jason Terry":
+         "teamName"= {"Brooklyn Nets"},
+         "colors" : ["Black", "White"],
+         "players": {
+            "Alan Anderson": {
+                "number":0,
+                "shoe":16,
+                "points":22,
+                "rebounds":12,
+                "assists":12,
+                "steals":3,
+                "blocks":1,
+                "slamDunks":1,
+                },
+            "Reggie Evans": {
+               
+                
+               },
+              
+            
+                },
         },
     }
     away: {
